c91c88c15024dec8048a0f83bde70e23f95dbb14
fabferlin <fabferlin@gmail.com>
2021-02-24 16:42:53 -0300

    login and logout with okta and oauth2

---
+++ src/test/java/com/example/sociallogin/SocialLoginWebClient/SocialLoginWebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sociallogin.SocialLoginWebClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginWebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
